ac064c5d94d76e1f5cd8e0bd1307d6ef466de588
martin3093 <martinkbz58@gmail.com>
2025-04-04 11:17:10 -0300

    Hora 2: Stateless vs. Stateful Widgets
    Usamos setState para decirle a Flutter que el
     estado cambió y que debe redibujar la pantalla
      Sin setState, el número no se actualizaría aunque
      la variable cambie

--- lib/views/pantalla_principal.dart
+++ lib/views/pantalla_principal.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class PantallaPrincipal extends StatelessWidget {
+class PantallaPrincipal extends StatefulWidget {
   const PantallaPrincipal({super.key}); // Constructor constante agregado
+
+  @override
+  State<PantallaPrincipal> createState() => _PantallaPrincipalState();
+}
+
+class _PantallaPrincipalState extends State<PantallaPrincipal> {
+  int _contador = 0;
+
+  void _incrementarContador() {
+    setState(() {
+      _contador++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +27,7 @@ class PantallaPrincipal extends StatelessWidget {
           children: [
             Container(
               /*agrega un resaldador de dimension 20 al texto */
-              color: Colors.green, // Modificación 1
+              color: Colors.blueAccent, // Modificación 1
               padding: EdgeInsets.all(20), // Modificación 2
               child: const Text(
                 'Hola, Flutter',
@@ -24,6 +37,15 @@ class PantallaPrincipal extends StatelessWidget {
             ElevatedButton(
               onPressed: () {},
               child: const Text('Presionar'), // const agregado al Text
+            ),
+            Text(
+              'Veces presionado: $_contador', // Modificación 1
+              style: TextStyle(color: Colors.blue), // Modificación 3
+            ),
+            SizedBox(height: 30), // Modificación 2
+            ElevatedButton(
+              onPressed: _incrementarContador,
+              child: Text('Toca aquí'),
             ),
           ],
         ),
